ed6f4efcdb3ee5e4371457c413cdc734b875e94c
Dhruvdip-Padhiyar <dhruvdippadhiyar@gmail.com>
2022-09-09 17:33:36 +0530

    crud for employee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Empolyee from "./components/Empolyee";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [empolyeeData, setEmployeeData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://dummy.restapiexample.com/api/v1/employees"
+    );
+    const data = await response.json();
+    localStorage.setItem("employess", JSON.stringify(data));
+    return data;
+  };
+
+  const addEmployee = async (employee) => {
+    const response = await fetch(
+      "https://dummy.restapiexample.com/api/v1/create",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(employee),
+      }
+    );
+    const data = await response.json();
+
+    setEmployeeData([...employee, data]);
+  };
+
+  const deleteEmployee = async (id) => {
+    await fetch(`https://dummy.restapiexample.com/api/v1/delete/${id}`, {
+      method: "DELETE",
+    });
+    console.log(empolyeeData.data.filter((employe) => employe.id !== id));
+
+    setEmployeeData(empolyeeData.data.filter((employe) => employe.id !== id));
+  };
+
+  useEffect(() => {
+    const getEmployees = async () => {
+      const employees = await fetchData();
+      setEmployeeData(employees);
+      console.log(JSON.parse(localStorage.getItem("employess")));
+    };
+    getEmployees();
+  }, [empolyeeData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Empolyee
+                addEmployee={addEmployee}
+                employeeData={empolyeeData}
+                handdleDelete={deleteEmployee}
+              />
+            }
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
